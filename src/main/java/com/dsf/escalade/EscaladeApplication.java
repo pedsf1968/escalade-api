72f3ec5cac3673309343f5dd3ff921b37244c4bb
PEDSF <pe.dsf@free.fr>
2019-11-01 11:08:51 +0100

    Création projet escalade

---
+++ src/main/java/com/dsf/escalade/EscaladeApplication.java
@@ -0,0 +1,13 @@
+package com.dsf.escalade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscaladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscaladeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsf/escalade/EscaladeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsf.escalade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscaladeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
