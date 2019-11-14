fc317f321cae0c63eff151a71c1fb9733817f6f9
PEDSF <pe.dsf@free.fr>
2019-11-14 20:07:10 +0100

    correction nav sec:authorize

--- src/main/java/com/dsf/escalade/controller/MvcConfig.java
+++ src/main/java/com/dsf/escalade/controller/MvcConfig.java
@@ -8,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
    public void addViewControllers(ViewControllerRegistry registry) {
+      registry.addViewController("/").setViewName("home");
       registry.addViewController("/home").setViewName("home");
-      //registry.addViewController("/").setViewName("home");
       registry.addViewController("/hello").setViewName("hello");
       registry.addViewController("/login").setViewName("login");
       registry.addViewController("/topo/topo-list").setViewName("topo/topo-list");
+      registry.addViewController("/topo/topo-update").setViewName("topo/topo-update");
 
    }
 
--- src/main/java/com/dsf/escalade/controller/TopoController.java
+++ src/main/java/com/dsf/escalade/controller/TopoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -42,6 +43,50 @@ public class TopoController {
         }
 
        model.addAttribute("siteList", listSite);
+        model.addAttribute("topoList", listTopo);
+
+        return "topo/topo-list";
+    }
+
+    @GetMapping("/edittopo/{id}")
+    public String editTopo(@PathVariable("id") Integer id, Model model) {
+        Site site = siteDao.findById(id)
+              .orElseThrow(() -> new IllegalArgumentException("Invalid site Id:" + id));
+
+        Topo topo = topoDao.findById(id)
+              .orElseThrow(() -> new IllegalArgumentException("Invalid topo Id:" + id));
+
+        model.addAttribute("site", site);
+        model.addAttribute("topo", site);
+        return "topo/topo-update";
+    }
+
+    @GetMapping("/deletetopo/{id}")
+    public String deleteTopo(@PathVariable("id") Integer id, Model model) {
+        Topo topo = topoDao.findById(id)
+              .orElseThrow(() -> new IllegalArgumentException("Invalid topo Id:" + id));
+        topoDao.delete(topo);
+
+        Site site = siteDao.findById(id)
+              .orElseThrow(() -> new IllegalArgumentException("Invalid site Id:" + id));
+        siteDao.delete(site);
+        listTopo( model);
+
+        List<Site> listSite = new ArrayList<Site>();
+        List<Topo> listTopo;
+
+        listTopo = topoDao.findAll();
+
+        for( Topo t:listTopo){
+            listSite.add(siteDao.getOne(t.getId()));
+            log.info("\n\n INFO topo :"+ t.toString());
+        }
+
+        for (Site s:listSite){
+            log.info("\n\n INFO site :"+ s.toString());
+        }
+
+        model.addAttribute("siteList", listSite);
         model.addAttribute("topoList", listTopo);
 
         return "topo/topo-list";
