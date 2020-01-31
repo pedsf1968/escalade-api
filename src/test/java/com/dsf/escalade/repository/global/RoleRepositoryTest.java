ab78a0c713bbc3e18dac411c57cbd80dbc2a0007
paul-emmanuel.dos-sa <GitHub1418$>
2020-01-31 13:14:39 +0100

    Corretion du plantage des tests en déplaçant des properties du bootstrap dans application.properties

--- src/test/java/com/dsf/escalade/repository/global/RoleRepositoryTest.java
+++ src/test/java/com/dsf/escalade/repository/global/RoleRepositoryTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
